6f01583fdb6fd60ebb28bcfff20aa0afe527cfc0
VIA <via256@mail.com>
2025-06-29 00:30:29 -0700

    establish some dll independance from original game build
    when possible. when being built, dlls reference other buildable dlls instead of the pre-supplied ones in the Mars Explorere directory

--- build_all.bat
+++ build_all.bat
@@ -1,27 +1,27 @@
-@echo on
+@echo off
+
+set "flags=/p:Configuration=Debug /p:Platform=x86"
 
 echo -------------------
 echo   CLEANING BUILDS
 echo -------------------
 
-msbuild /t:Clean "Assembly - CSharp\Assembly---CSharp.csproj"
+msbuild /t:Clean %flags% "Assembly - CSharp\Assembly---CSharp.csproj"
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean "Assembly - CSharp - first pass\Assembly---CSharp---first-pass.csproj"
+msbuild /t:Clean %flags% "Assembly - CSharp - first pass\Assembly---CSharp---first-pass.csproj"
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean "Assembly - UnityScript\Assembly---UnityScript.csproj"
+msbuild /t:Clean %flags% "Assembly - UnityScript\Assembly---UnityScript.csproj"
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean "Assembly - UnityScript - first pass\Assembly---UnityScript---first-pass.csproj"
+msbuild /t:Clean %flags% "Assembly - UnityScript - first pass\Assembly---UnityScript---first-pass.csproj"
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean "Ionic.Zlib\Ionic.Zlib.csproj"
+msbuild /t:Clean %flags% "Ionic.Zlib\Ionic.Zlib.csproj"
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean "TerrainControllerData\TerrainControllerData.csproj"
+msbuild /t:Clean %flags% "TerrainControllerData\TerrainControllerData.csproj"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo ----------------
 echo   BUILDING ALL
 echo ----------------
-
-set "flags=/p:Configuration=Debug /p:Platform=x86"
 
 msbuild %flags% "Assembly - CSharp - first pass\Assembly---CSharp---first-pass.csproj"
 if %errorlevel% neq 0 exit /b %errorlevel%
