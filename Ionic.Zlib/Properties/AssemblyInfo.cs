9afe4435c4787001ad4fa20165179094c48882ef
VIA <via256@mail.com>
2025-06-28 19:58:38 -0700

    added ionic.zlib

---
+++ Ionic.Zlib/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+[assembly: AssemblyProduct("DotNetZlib Library")]
+[assembly: AssemblyTitle("Ionic's Managed Zlib")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCopyright("Copyright © Dino Chiesa 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyDescription("library for Deflate and ZLIB compression. http://www.codeplex.com/DotNetZip (Flavor=Retail)")]
+[assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyFileVersion("1.8.4.24")]
+[assembly: AssemblyVersion("1.8.4.24")]
