f4393223f031ee872f5c13dc08a24fa9a16b8615
VIA <via256@mail.com>
2025-09-23 14:24:11 -0700

    Make HoverThrustClustOfDust.cs conform to style

--- Assembly - UnityScript/HoverThrustClustOfDust.cs
+++ Assembly - UnityScript/HoverThrustClustOfDust.cs
@@ -2,28 +2,24 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class HoverThrustClustOfDust : MonoBehaviour
-{
+public class HoverThrustClustOfDust : MonoBehaviour {
 	public LayerMask thrustMask;
 
-	public HoverThrustClustOfDust()
-	{
+	public HoverThrustClustOfDust(){
 		thrustMask = -1;
 	}
 
-	public void FixedUpdate()
-	{
-		if (transform.position.y < Game.Settings.lavaAlt + 15f || Physics.Raycast(transform.position, Vector3.down, 15f, thrustMask))
-		{
+	public void FixedUpdate(){
+		if (
+			transform.position.y < Game.Settings.lavaAlt + 15f ||
+			Physics.Raycast(transform.position, Vector3.down, 15f, thrustMask)
+		){
 			particleEmitter.emit = true;
 		}
-		else
-		{
+		else {
 			particleEmitter.emit = false;
 		}
 	}
 
-	public void Main()
-	{
-	}
+	public void Main(){}
 }
