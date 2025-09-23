4f26208f717cc0845ef5c764a93adc7cbf117f99
VIA <via256@mail.com>
2025-09-23 14:12:18 -0700

    Make FloorController.cs more readable

--- Assembly - UnityScript/FloorController.cs
+++ Assembly - UnityScript/FloorController.cs
@@ -3,21 +3,24 @@ using Boo.Lang.Runtime;
 using UnityEngine;
 
 [Serializable]
-public class FloorController : MonoBehaviour
-{
+public class FloorController : MonoBehaviour {
 	public WhirldObject whirldObject;
-
 	public GameObject floorObject;
 
-	public void OnSceneGenerated()
-	{
-		if ((bool)whirldObject && RuntimeServices.ToBool(whirldObject.@params["Texture"]) && (bool)floorObject)
-		{
-			floorObject.renderer.material.mainTexture = (Texture)RuntimeServices.Coerce(whirldObject.@params["Texture"], typeof(Texture));
+	public void OnSceneGenerated(){
+		if (
+			!whirldObject ||
+			!RuntimeServices.ToBool(whirldObject.@params["Texture"]) ||
+			!floorObject
+		){
+			return;
 		}
+
+		floorObject.renderer.material.mainTexture = (Texture)RuntimeServices.Coerce(
+			whirldObject.@params["Texture"],
+			typeof(Texture)
+		);
 	}
 
-	public void Main()
-	{
-	}
+	public void Main(){}
 }
