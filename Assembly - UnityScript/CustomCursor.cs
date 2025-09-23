a7e584e243ad653153021be5ef722177cacc9fbf
VIA <via256@mail.com>
2025-09-23 14:07:28 -0700

    Make CustomCursor.cs more readable

--- Assembly - UnityScript/CustomCursor.cs
+++ Assembly - UnityScript/CustomCursor.cs
@@ -2,22 +2,21 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class CustomCursor : MonoBehaviour
-{
+public class CustomCursor : MonoBehaviour {
 	public Texture2D cursor;
-
 	public Vector2 cursorOffset;
 
-	public void OnGUI()
-	{
-		if (Screen.lockCursor)
-		{
+	public void OnGUI(){
+		if (Screen.lockCursor){
 			GUI.depth = -999;
-			GUI.Label(new Rect((float)(Screen.width / 2) - cursorOffset.x, (float)(Screen.height / 2) - cursorOffset.y, cursor.width, cursor.height), cursor);
+			GUI.Label(new Rect(
+				(float)(Screen.width / 2) - cursorOffset.x,
+				(float)(Screen.height / 2) - cursorOffset.y,
+				cursor.width,
+				cursor.height
+			), cursor);
 		}
 	}
 
-	public void Main()
-	{
-	}
+	public void Main(){}
 }
