5b8503ba39c9897677cbb0b8c53bca3a598de8ce
VIA <via256@mail.com>
2025-09-23 14:15:34 -0700

    Make GUIPanel.cs conform to style

--- Assembly - UnityScript/GUIPanel.cs
+++ Assembly - UnityScript/GUIPanel.cs
@@ -2,30 +2,21 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class GUIPanel
-{
+public class GUIPanel{
 	public string name;
-
 	public bool active;
-
 	public bool open;
-
 	public bool important;
-
+	
 	public int minHeight;
-
 	public int maxHeight;
-
 	public float curHeight;
-
 	public float desHeight;
-
+	
 	public Vector2 scrollPos;
-
 	public float openTime;
 
-	public GUIPanel()
-	{
+	public GUIPanel(){
 		active = true;
 		minHeight = 300;
 	}
