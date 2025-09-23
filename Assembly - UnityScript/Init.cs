2440d0ac9186ea8aaccd88adc53953c8bba35b16
VIA <via256@mail.com>
2025-09-23 15:29:30 -0700

    Assimilate Init.cs

--- Assembly - UnityScript/Init.cs
+++ Assembly - UnityScript/Init.cs
@@ -2,27 +2,20 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class Init : MonoBehaviour
-{
+public class Init : MonoBehaviour {
 	public GUIText txt;
 
-	public void Update()
-	{
-		checked
-		{
-			float streamProgressForLevel = Application.GetStreamProgressForLevel(Application.loadedLevel + 1);
-			if (streamProgressForLevel == 1f)
-			{
+	public void Update(){
+		checked {
+			float i = Application.GetStreamProgressForLevel(Application.loadedLevel + 1);
+			if (i == 1f){
 				Application.LoadLevel(Application.loadedLevel + 1);
 			}
-			else
-			{
-				txt.text = Mathf.RoundToInt(streamProgressForLevel * 100f) + "%";
+			else {
+				txt.text = Mathf.RoundToInt(i * 100f) + "%";
 			}
 		}
 	}
 
-	public void Main()
-	{
-	}
+	public void Main(){}
 }
