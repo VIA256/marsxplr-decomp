ee4b4f9b2d58bbee2f64f79d7b28cd1c05df5e1f
VIA <via256@mail.com>
2025-09-23 14:04:44 -0700

    Make CompilerGeneratedExtensions.cs conform to style

--- Assembly - UnityScript/CompilerGeneratedExtensions.cs
+++ Assembly - UnityScript/CompilerGeneratedExtensions.cs
@@ -2,28 +2,27 @@ using System;
 using Boo.Lang;
 using CompilerGenerated;
 
-namespace System.Runtime.CompilerServices
-{
+namespace System.Runtime.CompilerServices{
 	[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class
 		 | AttributeTargets.Method)]
 	public sealed class ExtensionAttribute : Attribute { }
 }
 
-public static class CompilerGeneratedExtensions
-{
-	/*private CompilerGeneratedExtensions()
-	{
-	}*/
+public static class CompilerGeneratedExtensions {
+	/*private CompilerGeneratedExtensions(){}*/
 
 	[Extension]
-	public static IAsyncResult BeginInvoke(this __Lobby_OnGUI_0024callable0_0024274_261__ self, AsyncCallback callback)
-	{
+	public static IAsyncResult BeginInvoke(
+		this __Lobby_OnGUI_0024callable0_0024274_261__ self,
+		AsyncCallback callback
+	){
 		return self.BeginInvoke(callback, null);
 	}
 
 	[Extension]
-	public static IAsyncResult BeginInvoke(this __Lobby_OnGUI_0024callable0_0024274_261__ self)
-	{
+	public static IAsyncResult BeginInvoke(
+		this __Lobby_OnGUI_0024callable0_0024274_261__ self
+	){
 		return self.BeginInvoke(null, null);
 	}
 }
