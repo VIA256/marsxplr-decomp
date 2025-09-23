061dd616e4a53f76831e9505f77ae48693820093
VIA <via256@mail.com>
2025-09-23 14:29:31 -0700

    Assimilate HoverThrustMoonOrBust.cs

--- Assembly - UnityScript/HoverThrustMoonOrBust.cs
+++ Assembly - UnityScript/HoverThrustMoonOrBust.cs
@@ -5,36 +5,31 @@ using UnityEngine;
 public class HoverThrustMoonOrBust : MonoBehaviour
 {
 	public Vehicle vehicle;
-
 	public ParticleRenderer particleRenderer;
 
-	public void Start()
-	{
+	public void Start(){
 		vehicle = (Vehicle)gameObject.transform.root.gameObject.GetComponentInChildren(typeof(Vehicle));
 		particleRenderer = (ParticleRenderer)gameObject.GetComponent("ParticleRenderer");
 	}
 
-	public void FixedUpdate()
-	{
-		float x = (float)((vehicle.input.y != 0f) ? 5 : 2) * vehicle.input.x;
+	public void FixedUpdate(){
+		float x = (float)((vehicle.input.y == 0f) ? 2 : 5) * vehicle.input.x;
 		Vector3 localVelocity = particleEmitter.localVelocity;
-		float num = (localVelocity.x = x);
-		Vector3 vector = (particleEmitter.localVelocity = localVelocity);
-		float z = Mathf.Min(-10f * vehicle.input.y, 0.5f * -1f);
-		Vector3 localVelocity2 = particleEmitter.localVelocity;
-		float num2 = (localVelocity2.z = z);
-		Vector3 vector3 = (particleEmitter.localVelocity = localVelocity2);
-		if (!(particleEmitter.localVelocity.z < -1f))
-		{
+		localVelocity.x = x
+		particleEmitter.localVelocity = localVelocity
+		
+		float z = Mathf.Min(-10f * vehicle.input.y, -0.5f);
+		localVelocity = particleEmitter.localVelocity;
+		localVelocity.z = z;
+		particleEmitter.localVelocity = localVelocity;
+		
+		if (particleEmitter.localVelocity.z >= -1f){
 			particleRenderer.particleRenderMode = ParticleRenderMode.Billboard;
 		}
-		else
-		{
+		else {
 			particleRenderer.particleRenderMode = ParticleRenderMode.Stretch;
 		}
 	}
 
-	public void Main()
-	{
-	}
+	public void Main(){}
 }
