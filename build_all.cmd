3961fdf446181a90f4ef3ef00d6f01e537b9d15f
VIA <via256@mail.com>
2025-09-23 17:35:43 -0700

    seperate clean stage from build stage

--- build_all.cmd
+++ build_all.cmd
@@ -2,31 +2,6 @@
 
 set "flags=/p:Configuration=Debug /p:Platform=x86"
 
-echo -------------------
-echo   CLEANING BUILDS
-echo -------------------
-
-msbuild /t:Clean %flags% "Assembly - CSharp\Assembly---CSharp.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "Assembly - CSharp - first pass\Assembly---CSharp---first-pass.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "Assembly - UnityScript\Assembly---UnityScript.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "Assembly - UnityScript - first pass\Assembly---UnityScript---first-pass.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "Ionic.Zlib\Ionic.Zlib.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "TerrainControllerData\TerrainControllerData.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "UnityEngine\UnityEngine.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "Boo.Lang\Boo.Lang.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "UnityScript.Lang\UnityScript.Lang.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild /t:Clean %flags% "UnityDomainLoad\UnityDomainLoad.csproj"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 echo ----------------
 echo   BUILDING ALL
 echo ----------------
---
+++ clean_all.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+set "flags=/p:Configuration=Debug /p:Platform=x86"
+
+echo -------------------
+echo   CLEANING BUILDS
+echo -------------------
+
+msbuild /t:Clean %flags% "Assembly - CSharp\Assembly---CSharp.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "Assembly - CSharp - first pass\Assembly---CSharp---first-pass.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "Assembly - UnityScript\Assembly---UnityScript.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "Assembly - UnityScript - first pass\Assembly---UnityScript---first-pass.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "Ionic.Zlib\Ionic.Zlib.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "TerrainControllerData\TerrainControllerData.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "UnityEngine\UnityEngine.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "Boo.Lang\Boo.Lang.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "UnityScript.Lang\UnityScript.Lang.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild /t:Clean %flags% "UnityDomainLoad\UnityDomainLoad.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
