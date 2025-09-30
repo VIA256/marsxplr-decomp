02d3389275835651ca25dfffb05cfb0770f75bc0
VIA <via256@mail.com>
2025-09-30 10:24:53 -0700

    Update GUIPanel.cs

--- Game/Assembly - UnityScript/GUIPanel.cs
+++ Game/Assembly - UnityScript/GUIPanel.cs
@@ -5,21 +5,15 @@ using UnityEngine;
 public class GUIPanel
 {
 	public string name;
-	public bool active;
+	public bool active = true;
 	public bool open;
 	public bool important;
 	
-	public int minHeight;
+	public int minHeight = 300;
 	public int maxHeight;
 	public float curHeight;
 	public float desHeight;
 	
 	public Vector2 scrollPos;
 	public float openTime;
-
-	public GUIPanel()
-    {
-		active = true;
-		minHeight = 300;
-	}
 }
