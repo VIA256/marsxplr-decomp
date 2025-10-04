5626aebf6768e95713fb8e4db48927924a05963d
VIA <via256@mail.com>
2025-10-04 13:44:31 -0700

    Assimilate MiniMap.cs

--- Game/Assembly - UnityScript/MiniMap.cs
+++ Game/Assembly - UnityScript/MiniMap.cs
@@ -1,121 +1,107 @@
 using System;
 using UnityEngine;
-using UnityScript.Lang;
+using System.Collections;
+using System.Collections.Generic;
 
 [Serializable]
 [RequireComponent(typeof(Camera))]
 public class MiniMap : MonoBehaviour
 {
-	public float camHeight;
+	public float camHeight = 650;
+	public Terrain[] terrains;
 
-	public UnityScript.Lang.Array terrains;
-
-	private bool revertFogState;
-
-	private bool terrainLighting;
-
-	private int terrainLOD;
-
-	private int terrainTreeDistance;
-
-	private int terrainDetailDistance;
-
-	private int terrainBasemapDistance;
-
-	public MiniMap()
-	{
-		camHeight = 650f;
-		terrains = new UnityScript.Lang.Array();
-		revertFogState = false;
-		terrainLighting = false;
-		terrainLOD = 0;
-		terrainTreeDistance = 0;
-		terrainDetailDistance = 0;
-		terrainBasemapDistance = 0;
-	}
+	private bool revertFogState = false;
+    // /*UNUSED*/ private bool terrainLighting = false; //DRAGONHERE: TerrainLighting depricated, rewrite this code to use new QualitySettings system
+	private int terrainLOD = 0;
+	private float terrainTreeDistance = 0;
+	private float terrainDetailDistance = 0;
+	// /*UNUSED*/ private float terrainBasemapDistance = 0;
 
 	public void Update()
 	{
-		if (!Game.Settings.minimapAllowed || !Game.Settings.useMinimap || Game.Controller.loadingWorld || !Game.Player)
+		if (
+            !Game.Settings.minimapAllowed ||
+            !Game.Settings.useMinimap ||
+            Game.Controller.loadingWorld ||
+            !Game.Player)
 		{
 			camera.enabled = false;
 			return;
 		}
 		camera.enabled = true;
+
 		transform.position = Camera.main.transform.position;
-		float y = Game.Player.transform.position.y + camHeight;
-		Vector3 position = transform.position;
-		float num = (position.y = y);
-		Vector3 vector = (transform.position = position);
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(90f, Game.Player.transform.eulerAngles.y, 0f), Time.deltaTime);
+
+		Vector3 tpos = transform.position;
+        tpos.y = Game.Player.transform.position.y + camHeight;
+		transform.position = tpos;
+
+		transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            Quaternion.Euler(90f, Game.Player.transform.eulerAngles.y, 0f),
+            Time.deltaTime);
 	}
 
 	public void OnPreRender()
 	{
 		revertFogState = RenderSettings.fog;
 		RenderSettings.fog = false;
-		if (World.terrains == null || Extensions.get_length((System.Array)World.terrains) <= 0)
-		{
-			return;
-		}
-		if (World.terrains[0].lighting == TerrainLighting.Pixel)
-		{
-			terrainLighting = true;
-		}
-		else
-		{
-			terrainLighting = false;
-		}
-		terrainLOD = World.terrains[0].heightmapMaximumLOD;
-		checked
-		{
-			terrainTreeDistance = (int)World.terrains[0].treeDistance;
-			terrainDetailDistance = (int)World.terrains[0].detailObjectDistance;
-			int i = 0;
-			Terrain[] array = World.terrains;
-			for (int length = array.Length; i < length; i++)
-			{
-				if (Game.Settings.renderLevel > 4)
-				{
-					array[i].heightmapMaximumLOD = 3;
-				}
-				else if (Game.Settings.renderLevel > 3)
-				{
-					array[i].heightmapMaximumLOD = 4;
-				}
-				else
-				{
-					array[i].heightmapMaximumLOD = 5;
-				}
-				array[i].lighting = TerrainLighting.Lightmap;
-				array[i].treeDistance = 0f;
-				array[i].detailObjectDistance = 0f;
-			}
-		}
+
+        if (World.terrains != null && World.terrains.Length > 0)
+        {
+            //if(World.terrains[0].lighting == TerrainLighting.Pixel) terrainLightingEnabled = true;
+            //else terrainLightingEnabled = false;
+            //terrainBasemapDistance = World.terrains[0].basemapDistance;
+            terrainLOD = World.terrains[0].heightmapMaximumLOD;
+            terrainTreeDistance = World.terrains[0].treeDistance;
+            terrainDetailDistance = World.terrains[0].detailObjectDistance;
+            foreach (Terrain trn in World.terrains)
+            { 
+                //trn.basemapDistance = 1000;
+                if (Game.Settings.renderLevel > 4)
+                {
+                    trn.heightmapMaximumLOD = 3;
+                }
+                else if (Game.Settings.renderLevel > 3)
+                {
+                    trn.heightmapMaximumLOD = 4;
+                }
+                else trn.heightmapMaximumLOD = 5;
+                //trn.lighting = TerrainLighting.Lightmap;
+                trn.treeDistance = 0;
+                trn.detailObjectDistance = 0;
+            }
+        }
 	}
 
 	public void OnPostRender()
 	{
 		RenderSettings.fog = revertFogState;
-		if (World.terrains != null)
-		{
-			int i = 0;
-			Terrain[] array = World.terrains;
-			for (int length = array.Length; i < length; i = checked(i + 1))
-			{
-				array[i].lighting = ((!terrainLighting) ? TerrainLighting.Lightmap : TerrainLighting.Pixel);
-				array[i].treeDistance = terrainTreeDistance;
-				array[i].detailObjectDistance = terrainDetailDistance;
-				array[i].heightmapMaximumLOD = terrainLOD;
-			}
-		}
+
+        if (World.terrains != null)
+        {
+            foreach (Terrain trn in World.terrains)
+            {
+                trn.treeDistance = terrainTreeDistance;
+                trn.detailObjectDistance = terrainDetailDistance;
+                trn.heightmapMaximumLOD = terrainLOD;
+            }
+        }
 	}
 
 	public void OnGUI()
 	{
-		if (Game.Settings.minimapAllowed && Game.Settings.useMinimap && (bool)Game.Player)
-		{
-			camHeight = GUI.HorizontalSlider(new Rect((float)Screen.width * 0.01f + 25f, (float)checked(Screen.height - 20) - (float)Screen.height * 0.001f, (float)Screen.width * 0.25f - 50f, 20f), camHeight, 200f, 1300f);
-		}
+        if (
+            !Game.Settings.minimapAllowed ||
+            !Game.Settings.useMinimap ||
+            !Game.Player) return;
+        camHeight = GUI.HorizontalSlider(
+            new Rect(
+                Screen.width * 0.01f + 25f,
+                Screen.height - 20f - Screen.height * 0.001f,
+                Screen.width * 0.25f - 50f, 20f),
+            camHeight,
+            200f,
+            1300f);
 	}
 }
