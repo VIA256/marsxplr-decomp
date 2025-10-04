4d0e4755f94ed33c3ddbb1ab9956a393748ac7d0
VIA <via256@mail.com>
2025-10-04 00:11:45 -0700

    add stray markSection class back into Skidmarks.cs

--- Game/Assembly - UnityScript/Skidmarks.cs
+++ Game/Assembly - UnityScript/Skidmarks.cs
@@ -3,6 +3,17 @@ using Boo.Lang.Runtime;
 using UnityEngine;
 
 [Serializable]
+public class markSection
+{
+    public Vector3 pos;
+    public Vector3 normal;
+    public Vector3 posl;
+    public Vector3 posr;
+    public float intensity = 0.0f;
+    public int lastIndex = -1;
+}
+
+[Serializable]
 public class Skidmarks : MonoBehaviour
 {
 	private int maxMarks;
