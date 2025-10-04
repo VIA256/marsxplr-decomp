d72ffae7897d2c89f5169d821f6232e49a370b92
VIA <via256@mail.com>
2025-10-04 00:02:16 -0700

    Assimilate LobbyStarfield.cs

--- Game/Assembly - UnityScript/LobbyStarfield.cs
+++ Game/Assembly - UnityScript/LobbyStarfield.cs
@@ -1,31 +1,27 @@
 using System;
-using Boo.Lang.Runtime;
 using UnityEngine;
-using UnityScript.Lang;
 
 [Serializable]
 public class LobbyStarfield : MonoBehaviour
 {
 	public Lobby Lobby;
 
-	public void Start()
-	{
-	}
-
 	public void Update()
 	{
 		if (Application.loadedLevel == 1 && Lobby.GUIHide > 0f)
 		{
-			Color color = (Color)UnityRuntimeServices.Invoke(RuntimeServices.GetProperty(gameObject.GetComponent(typeof(ParticleRenderer)), "material"), "GetColor", new object[1] { "_TintColor" }, typeof(MonoBehaviour));
-			color.a = 0.5f - Lobby.GUIHide / 2f;
-			UnityRuntimeServices.Invoke(RuntimeServices.GetProperty(gameObject.GetComponent(typeof(ParticleRenderer)), "material"), "SetColor", new object[2] { "_TintColor", color }, typeof(MonoBehaviour));
+            Color col =
+                ((ParticleRenderer)gameObject.GetComponent(typeof(ParticleRenderer)))
+                .material
+                .GetColor("_TintColor");
+			col.a = 0.5f - Lobby.GUIHide / 2f;
+            ((ParticleRenderer)gameObject.GetComponent(typeof(ParticleRenderer))).material.SetColor("_TintColor", col);
 		}
 		else if (Application.loadedLevel > 1)
 		{
-			float y = Mathf.Min(Time.timeSinceLevelLoad, 5f) * -1f;
-			Vector3 worldVelocity = particleEmitter.worldVelocity;
-			float num = (worldVelocity.y = y);
-			Vector3 vector = (particleEmitter.worldVelocity = worldVelocity);
+			Vector3 pewv = particleEmitter.worldVelocity;
+			pewv.y = Mathf.Min(Time.timeSinceLevelLoad, 5f) * -1f;
+			particleEmitter.worldVelocity = pewv;
 			if (Time.timeSinceLevelLoad > 7f || QualitySettings.currentLevel < QualityLevel.Good)
 			{
 				particleEmitter.emit = false;
