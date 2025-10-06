af3fa11338ec0a9d0e3fc0c54c71e9390302c0ae
VIA <via256@mail.com>
2025-10-05 23:53:29 -0700

    Assimilate TankTrackSimple.cs

--- Game/Assembly - UnityScript/TankTrackSimple.cs
+++ Game/Assembly - UnityScript/TankTrackSimple.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 [Serializable]
 public class TankTrackSimple : MonoBehaviour
 {
-	public LayerMask terrainMask;
-
-	private RaycastHit hit;
-
-	private Transform myTransform;
-
-	private Vector3 linkPos;
-
-	public TankTrackSimple()
-	{
-		terrainMask = ~1 << 4;
-	}
+	public LayerMask terrainMask = ~1 << 4;
+	// /*UNUSED*/ private RaycastHit hit;
+    // /*UNUSED*/ private Transform myTransform;
+    // /*UNUSED*/ private Vector3 linkPos;
 
 	public void Start()
 	{
+        //myTransform = transform;
 	}
 
 	public void FixedUpdate()
 	{
+    	//if(linkPos == Vector3.zero) linkPos = myTransform.InverseTransformPoint(myTransform.position);
+
+        /*if (Physics.Raycast(linkPos + transform.TransformPoint(Vector3.up * 5), transform.parent.TransformDirection(Vector3.down), hit, 5.5, terrainMask)) {
+	        myTransform.position = hit.point;
+	        myTransform.LookAt(hit.point + hit.normal);
+        }
+        else {*/
+	        //myTransform.position = myTransform.TransformPoint(linkPos);
+	        //myTransform.rotation = transform.parent.rotation;
+        //}
 	}
 }
