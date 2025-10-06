ff3d17499d942e5b80b19edad8f4675f6574917a
VIA <via256@mail.com>
2025-10-05 23:11:55 -0700

    Assimilate TankMe.cs

--- Game/Assembly - UnityScript/TankMe.cs
+++ Game/Assembly - UnityScript/TankMe.cs
@@ -1,26 +1,17 @@
 using System;
-using Boo.Lang.Runtime;
 using UnityEngine;
 
 [Serializable]
 public class TankMe : MonoBehaviour
 {
 	public Vehicle vehicle;
-
-	public bool bellyup;
-
-	public TankMe()
-	{
-		bellyup = false;
-	}
+	public bool bellyup = false;
 
 	public void FixedUpdate()
 	{
-		if (vehicle.myRigidbody.isKinematic)
-		{
-			return;
-		}
-		if (vehicle.brakes && vehicle.myRigidbody.velocity.magnitude < 3f)
+        if (vehicle.myRigidbody.isKinematic) return; //We are materializing, don't try to manipulate physics
+		
+        if (vehicle.brakes && vehicle.myRigidbody.velocity.magnitude < 3f)
 		{
 			vehicle.myRigidbody.velocity = Vector3.zero;
 			vehicle.myRigidbody.angularVelocity = Vector3.zero;
@@ -38,6 +29,7 @@ public class TankMe : MonoBehaviour
 			vehicle.myRigidbody.angularDrag = 2f;
 			vehicle.myRigidbody.drag = 0.01f;
 		}
+
 		if (bellyup && Vector3.Angle(transform.up, Vector3.up) < 35f)
 		{
 			bellyup = false;
@@ -46,30 +38,28 @@ public class TankMe : MonoBehaviour
 
 	public void OnCollisionStay(Collision collision)
 	{
-		if (vehicle.zorbBall)
-		{
-			return;
-		}
-		int i = 0;
-		ContactPoint[] contacts = collision.contacts;
-		for (int length = contacts.Length; i < length; i = checked(i + 1))
-		{
-			if (RuntimeServices.EqualityOperator(RuntimeServices.GetProperty(RuntimeServices.GetProperty(contacts[i].otherCollider, "gameObject"), "layer"), 0))
-			{
-				if (!bellyup && vehicle.myRigidbody.velocity.sqrMagnitude < 20f && vehicle.myRigidbody.angularVelocity.sqrMagnitude < 5f && Vector3.Angle(transform.up, contacts[i].normal) > 130f)
-				{
-					bellyup = true;
-				}
-				if (bellyup)
-				{
-					vehicle.myRigidbody.AddForce(Vector3.up * 5000f);
-					vehicle.myRigidbody.AddTorque(Vector3.Cross(transform.up, Vector3.up) * 100000f, ForceMode.Acceleration);
-				}
-			}
-		}
+        if (vehicle.zorbBall) return;
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if (contact.otherCollider.gameObject.layer != 0) continue;
+            if (
+                bellyup == false &&
+                vehicle.myRigidbody.velocity.sqrMagnitude < 20 &&
+                vehicle.myRigidbody.angularVelocity.sqrMagnitude < 5 &&
+                Vector3.Angle(transform.up, contact.normal) > 130)
+            {
+                bellyup = true;
+            }
+
+            if (bellyup)
+            {
+                vehicle.myRigidbody.AddForce(Vector3.up * 5000f);
+                vehicle.myRigidbody.AddTorque(
+                    Vector3.Cross(transform.up, Vector3.up) * 100000f,
+                    ForceMode.Acceleration);
+            }
+        }
 	}
 
-	public void OnSetSpecialInput()
-	{
-	}
+	public void OnSetSpecialInput() {}
 }
