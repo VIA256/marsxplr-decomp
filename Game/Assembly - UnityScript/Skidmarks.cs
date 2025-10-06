bdc2fa33d55fdcfe85aefdf09f6c61411879e31d
VIA <via256@mail.com>
2025-10-05 19:28:57 -0700

    assimilate Skidmarks.cs

--- Game/Assembly - UnityScript/Skidmarks.cs
+++ Game/Assembly - UnityScript/Skidmarks.cs
@@ -1,10 +1,10 @@
 using System;
-using Boo.Lang.Runtime;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 
+//data structure describing a section of skidmarks
 [Serializable]
 public class markSection
 {
@@ -19,47 +19,34 @@ public class markSection
 [Serializable]
 public class Skidmarks : MonoBehaviour
 {
-	private int maxMarks;
+    //maximal number of skidmarks
+	private int maxMarks = 256;
 
-	public float markWidth;
+    //width of skid marks
+	public float markWidth = 0.225f;
 
-	private float updateRate;
+    //time interval new mesh segments are generated in
+    //the lower this value, the smoother the generated tracks
+	private float updateRate = 0.2f;
 
-	private int indexShift;
-
-	private int numMarks;
-
-	private float updateTime;
-
-	private bool newTrackFlag;
-
-	private bool updateMeshFlag;
-
+	private int indexShift = 0;
+	private int numMarks = 0;
+	private float updateTime = 0.0f;
+	private bool newTrackFlag = true;
+	private bool updateMeshFlag = true;
 	private MeshFilter meshFilter;
-
 	private Mesh mesh;
 
 	public markSection[] skidmarks;
 
-	public Skidmarks()
-	{
-		maxMarks = 256;
-		markWidth = 0.225f;
-		updateRate = 0.2f;
-		indexShift = 0;
-		numMarks = 0;
-		updateTime = 0f;
-		newTrackFlag = true;
-		updateMeshFlag = true;
-	}
-
+    // Use this for initialization
 	public void Start()
 	{
+        //create structures
 		skidmarks = new markSection[maxMarks];
-		for (int i = 0; i < maxMarks; i = checked(i + 1))
+		for (int i = 0; i < maxMarks; i++)
 		{
-			markSection[] array = skidmarks;
-			array[RuntimeServices.NormalizeArrayIndex(array, i)] = new markSection();
+            skidmarks[i] = new markSection();
 		}
 		meshFilter = (MeshFilter)gameObject.GetComponent(typeof(MeshFilter));
 		mesh = meshFilter.mesh;
@@ -69,121 +56,133 @@ public class Skidmarks : MonoBehaviour
 		}
 	}
 
+    //called by the car script to add a skid mark at position pos with the supplied normal.
+    //transparency can be specified in the intensity parameter. Connects to the track segment 
+    //indexed by lastIndex (or it won't display if lastIndex is -1). returns an index value 
+    //which can be passed as lastIndex to the next AddSkidMark call
 	public int AddSkidMark(Vector3 pos, Vector3 normal, float intensity, int lastIndex)
 	{
 		intensity = Mathf.Clamp01(intensity);
-		int num = numMarks;
+
+        //get index for new segment
+		int currIndex = numMarks;
+
+        //reuse lastIndex if we don't need to create a new one this frame
 		if (lastIndex != -1 && !newTrackFlag)
 		{
-			num = lastIndex;
+			currIndex = lastIndex;
 		}
-		markSection[] array = skidmarks;
-		markSection markSection2 = array[RuntimeServices.NormalizeArrayIndex(array, num % maxMarks)];
-		markSection2.pos = pos + normal * 0.05f - transform.position;
-		markSection2.normal = normal;
-		markSection2.intensity = intensity;
+
+        //setup skidmark structure
+        markSection curr = skidmarks[currIndex % maxMarks];
+		curr.pos = pos + normal * 0.05f - transform.position;
+		curr.normal = normal;
+		curr.intensity = intensity;
+
 		if (lastIndex == -1 || newTrackFlag)
 		{
-			markSection2.lastIndex = lastIndex;
+			curr.lastIndex = lastIndex;
 		}
-		if (markSection2.lastIndex != -1)
+
+        //if we have a valid lastIndex, get positions for marks
+		if (curr.lastIndex != -1)
 		{
-			markSection[] array2 = skidmarks;
-			markSection markSection3 = array2[RuntimeServices.NormalizeArrayIndex(array2, markSection2.lastIndex % maxMarks)];
-			Vector3 lhs = markSection2.pos - markSection3.pos;
-			Vector3 normalized = Vector3.Cross(lhs, normal).normalized;
-			markSection2.posl = markSection2.pos + normalized * markWidth * 0.5f;
-			markSection2.posr = markSection2.pos - normalized * markWidth * 0.5f;
-			if (markSection3.lastIndex == -1)
+            markSection last = skidmarks[curr.lastIndex % maxMarks];
+            Vector3 dir = curr.pos - last.pos;
+			Vector3 normalized = Vector3.Cross(dir, normal).normalized;
+
+			curr.posl = curr.pos + normalized * markWidth * 0.5f;
+			curr.posr = curr.pos - normalized * markWidth * 0.5f;
+
+			if (last.lastIndex == -1)
 			{
-				markSection3.posl = markSection2.pos + normalized * markWidth * 0.5f;
-				markSection3.posr = markSection2.pos - normalized * markWidth * 0.5f;
+				last.posl = curr.pos + normalized * markWidth * 0.5f;
+				last.posr = curr.pos - normalized * markWidth * 0.5f;
 			}
 		}
-		checked
+		if (lastIndex == -1 || newTrackFlag)
 		{
-			if (lastIndex == -1 || newTrackFlag)
-			{
-				numMarks++;
-			}
-			updateMeshFlag = true;
-			return num;
+			numMarks++;
 		}
+		updateMeshFlag = true;
+		return currIndex;
 	}
 
+    //regenerate the skidmarks mesh
 	public void UpdateMesh()
 	{
-		int num = 0;
-		checked
+        //count visible segments
+		int segmentCount = 0;
+		for (int i = 0; i < numMarks && i < maxMarks; i++)
 		{
-			for (int i = 0; i < numMarks && i < maxMarks; i++)
-			{
-				markSection[] array = skidmarks;
-				if (array[RuntimeServices.NormalizeArrayIndex(array, i)].lastIndex != -1)
-				{
-					markSection[] array2 = skidmarks;
-					if (array2[RuntimeServices.NormalizeArrayIndex(array2, i)].lastIndex > numMarks - maxMarks)
-					{
-						num++;
-					}
-				}
-			}
-			Vector3[] array3 = new Vector3[num * 4];
-			Vector3[] array4 = new Vector3[num * 4];
-			Color[] array5 = new Color[num * 4];
-			Vector2[] array6 = new Vector2[num * 4];
-			int[] array7 = new int[num * 6];
-			num = 0;
-			for (int i = 0; i < numMarks && i < maxMarks; i++)
-			{
-				markSection[] array8 = skidmarks;
-				if (array8[RuntimeServices.NormalizeArrayIndex(array8, i)].lastIndex != -1)
-				{
-					markSection[] array9 = skidmarks;
-					if (array9[RuntimeServices.NormalizeArrayIndex(array9, i)].lastIndex > numMarks - maxMarks)
-					{
-						markSection[] array10 = skidmarks;
-						markSection markSection2 = array10[RuntimeServices.NormalizeArrayIndex(array10, i)];
-						markSection[] array11 = skidmarks;
-						markSection markSection3 = array11[RuntimeServices.NormalizeArrayIndex(array11, unchecked(markSection2.lastIndex % maxMarks))];
-						array3[RuntimeServices.NormalizeArrayIndex(array3, num * 4 + 0)] = markSection3.posl;
-						array3[RuntimeServices.NormalizeArrayIndex(array3, num * 4 + 1)] = markSection3.posr;
-						array3[RuntimeServices.NormalizeArrayIndex(array3, num * 4 + 2)] = markSection2.posl;
-						array3[RuntimeServices.NormalizeArrayIndex(array3, num * 4 + 3)] = markSection2.posr;
-						array4[RuntimeServices.NormalizeArrayIndex(array4, num * 4 + 0)] = markSection3.normal;
-						array4[RuntimeServices.NormalizeArrayIndex(array4, num * 4 + 1)] = markSection3.normal;
-						array4[RuntimeServices.NormalizeArrayIndex(array4, num * 4 + 2)] = markSection2.normal;
-						array4[RuntimeServices.NormalizeArrayIndex(array4, num * 4 + 3)] = markSection2.normal;
-						array5[RuntimeServices.NormalizeArrayIndex(array5, num * 4 + 0)] = new Color(1f, 1f, 1f, markSection3.intensity);
-						array5[RuntimeServices.NormalizeArrayIndex(array5, num * 4 + 1)] = new Color(1f, 1f, 1f, markSection3.intensity);
-						array5[RuntimeServices.NormalizeArrayIndex(array5, num * 4 + 2)] = new Color(1f, 1f, 1f, markSection2.intensity);
-						array5[RuntimeServices.NormalizeArrayIndex(array5, num * 4 + 3)] = new Color(1f, 1f, 1f, markSection2.intensity);
-						array6[RuntimeServices.NormalizeArrayIndex(array6, num * 4 + 0)] = new Vector2(0f, 0f);
-						array6[RuntimeServices.NormalizeArrayIndex(array6, num * 4 + 1)] = new Vector2(1f, 0f);
-						array6[RuntimeServices.NormalizeArrayIndex(array6, num * 4 + 2)] = new Vector2(0f, 0f);
-						array6[RuntimeServices.NormalizeArrayIndex(array6, num * 4 + 3)] = new Vector2(1f, 0f);
-						array7[RuntimeServices.NormalizeArrayIndex(array7, num * 6 + 0)] = num * 4 + 0;
-						array7[RuntimeServices.NormalizeArrayIndex(array7, num * 6 + 1)] = num * 4 + 1;
-						array7[RuntimeServices.NormalizeArrayIndex(array7, num * 6 + 2)] = num * 4 + 2;
-						array7[RuntimeServices.NormalizeArrayIndex(array7, num * 6 + 3)] = num * 4 + 2;
-						array7[RuntimeServices.NormalizeArrayIndex(array7, num * 6 + 4)] = num * 4 + 1;
-						array7[RuntimeServices.NormalizeArrayIndex(array7, num * 6 + 5)] = num * 4 + 3;
-						num++;
-					}
-				}
-			}
-			mesh.Clear();
-			mesh.vertices = array3;
-			mesh.normals = array4;
-			mesh.triangles = array7;
-			mesh.colors = array5;
-			mesh.uv = array6;
-			updateMeshFlag = false;
+            if (
+                skidmarks[i].lastIndex != -1 &&
+                skidmarks[i].lastIndex > numMarks - maxMarks)
+            {
+                segmentCount++;
+            }
 		}
+
+        //create skidmark mesh coordinates
+		Vector3[] vertices = new Vector3[segmentCount * 4];
+		Vector3[] normals = new Vector3[segmentCount * 4];
+		Color[] colors = new Color[segmentCount * 4];
+		Vector2[] uvs = new Vector2[segmentCount * 4];
+		int[] triangles = new int[segmentCount * 6];
+		segmentCount = 0;
+		for (int i = 0; i < numMarks && i < maxMarks; i++)
+		{
+            if (
+                skidmarks[i].lastIndex != -1 &&
+                skidmarks[i].lastIndex > numMarks - maxMarks)
+            {
+                markSection curr = skidmarks[i];
+                markSection last = skidmarks[curr.lastIndex % maxMarks];
+
+                vertices[segmentCount * 4 + 0] = last.posl;
+                vertices[segmentCount * 4 + 1] = last.posr;
+                vertices[segmentCount * 4 + 2] = curr.posl;
+                vertices[segmentCount * 4 + 3] = curr.posr;
+
+                normals[segmentCount * 4 + 0] = last.normal;
+                normals[segmentCount * 4 + 1] = last.normal;
+                normals[segmentCount * 4 + 2] = curr.normal;
+                normals[segmentCount * 4 + 3] = curr.normal;
+
+                colors[segmentCount * 4 + 0] = new Color(1f, 1f, 1f, last.intensity);
+                colors[segmentCount * 4 + 1] = new Color(1f, 1f, 1f, last.intensity);
+                colors[segmentCount * 4 + 2] = new Color(1f, 1f, 1f, curr.intensity);
+                colors[segmentCount * 4 + 3] = new Color(1f, 1f, 1f, curr.intensity);
+
+                uvs[segmentCount * 4 + 0] = new Vector2(0f, 0f);
+                uvs[segmentCount * 4 + 1] = new Vector2(1f, 0f);
+                uvs[segmentCount * 4 + 2] = new Vector2(0f, 0f);
+                uvs[segmentCount * 4 + 3] = new Vector2(1f, 0f);
+
+                triangles[segmentCount * 6 + 0] = segmentCount * 4 + 0;
+                triangles[segmentCount * 6 + 1] = segmentCount * 4 + 1;
+                triangles[segmentCount * 6 + 2] = segmentCount * 4 + 2;
+
+                triangles[segmentCount * 6 + 3] = segmentCount * 4 + 2;
+                triangles[segmentCount * 6 + 4] = segmentCount * 4 + 1;
+                triangles[segmentCount * 6 + 5] = segmentCount * 4 + 3;
+                segmentCount++;
+            }
+		}
+
+        //update mesh
+		mesh.Clear();
+		mesh.vertices = vertices;
+		mesh.normals = normals;
+		mesh.triangles = triangles;
+		mesh.colors = colors;
+		mesh.uv = uvs;
+		updateMeshFlag = false;
 	}
 
 	public void Update()
 	{
+        //update mesh if skidmarks have changed since last frame
 		if (updateMeshFlag)
 		{
 			UpdateMesh();
@@ -192,6 +191,7 @@ public class Skidmarks : MonoBehaviour
 
 	public void FixedUpdate()
 	{
+        //set flag for creating new segments this frame if an update is pending
 		newTrackFlag = false;
 		updateTime += Time.deltaTime;
 		if (updateTime > updateRate)
