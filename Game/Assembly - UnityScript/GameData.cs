aedc82e52abb2a3f1e83f35af1ff569974f2da94
VIA <via256@mail.com>
2025-10-18 22:41:08 -0700

    fix game version

--- Game/Assembly - UnityScript/GameData.cs
+++ Game/Assembly - UnityScript/GameData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [Serializable]
 public class GameData : MonoBehaviour
 {
-	public static float gameVersion = 2.3f;
+	public static float gameVersion = 2.22f;
 	public static float serverVersion = 0.2f;
 	public static string gameName = "marsxplr";
 	public static string userName = "";
