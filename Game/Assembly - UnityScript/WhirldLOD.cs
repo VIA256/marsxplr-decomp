f869dac1bffac15d5ce7239b688236351e253e97
VIA <via256@mail.com>
2025-10-20 16:45:51 -0700

    Assimilate WhirldLOD.cs

--- Game/Assembly - UnityScript/WhirldLOD.cs
+++ Game/Assembly - UnityScript/WhirldLOD.cs
@@ -1,27 +1,19 @@
 using System;
-using Boo.Lang.Runtime;
 using UnityEngine;
-using UnityScript.Lang;
 
 [Serializable]
 public class WhirldLOD : MonoBehaviour
 {
 	public GameObject[] lodObjs;
-
-	public int lodLevMax;
+	public int lodLevMax = 0;
 
 	[HideInInspector]
-	public int level;
-
-	private int lastLevel;
-
+	public int level = 0;
+	private int lastLevel = -1;
 	private float lodCheck;
 
 	public WhirldLOD()
 	{
-		lodLevMax = 0;
-		level = 0;
-		lastLevel = -1;
 		lodCheck = UnityEngine.Random.Range(30, 60) / 10;
 	}
 
@@ -32,32 +24,41 @@ public class WhirldLOD : MonoBehaviour
 
 	public void SetLOD()
 	{
-		checked
+		
+        //Level determined by distance to cam
+		if (lodLevMax == 0)
 		{
-			if (lodLevMax == 0)
-			{
-				level = ((World.lodDist <= 0) ? (Extensions.get_length((System.Array)lodObjs) - 1) : UnityBuiltins.parseInt(Mathf.Lerp(0f, Extensions.get_length((System.Array)lodObjs) - 1, Vector3.Distance(transform.position, Camera.main.transform.position) / (float)World.lodDist)));
-			}
-			else
-			{
-				level = (((int)QualitySettings.currentLevel < lodLevMax) ? 1 : 0);
-			}
-			if (lastLevel == level)
-			{
-				return;
-			}
-			BroadcastMessage("OnLOD", level, SendMessageOptions.DontRequireReceiver);
-			for (int i = 0; i < Extensions.get_length((System.Array)lodObjs); i++)
-			{
-				bool flag = i == level;
-				GameObject[] array = lodObjs;
-				if (array[RuntimeServices.NormalizeArrayIndex(array, i)].active != flag)
-				{
-					GameObject[] array2 = lodObjs;
-					array2[RuntimeServices.NormalizeArrayIndex(array2, i)].SetActiveRecursively(flag);
-				}
-			}
-			lastLevel = level;
+			level = (World.lodDist > 0 ?
+                (int)(Mathf.Lerp(
+                    0,
+                    lodObjs.Length - 1,
+                    Vector3.Distance(
+                        transform.position,
+                        Camera.main.transform.position) / World.lodDist)) :
+                lodObjs.Length - 1);
 		}
+
+        //Level determined directly by game quality level
+		else
+		{
+			level = (((int)QualitySettings.currentLevel >= lodLevMax) ? 0 : 1);
+		}
+
+        if (lastLevel != level)
+        {
+            BroadcastMessage(
+                "OnLOD",
+                level,
+                SendMessageOptions.DontRequireReceiver);
+            for (int i = 0; i < lodObjs.Length; i++)
+            {
+                bool desired = (i == level);
+                if (lodObjs[i].active != desired)
+                {
+                    lodObjs[i].SetActiveRecursively(desired);
+                }
+            }
+            lastLevel = level;
+        }
 	}
 }
