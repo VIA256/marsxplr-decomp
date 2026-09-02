92bf520e260ae1cddb273702da9aad550bf69aba
VIA256 <via256@mail.com>
2026-09-02 14:06:01 -0700

    oopsies

--- wafbuild/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x2010600
-tools = [{'tool': 'winres', 'tooldir': [], 'funs': None}, {'tool': 'c', 'tooldir': [], 'funs': None}, {'tool': 'cxx', 'tooldir': [], 'funs': None}, {'tool': 'msvc', 'tooldir': [], 'funs': None}, {'tool': 'compiler_c', 'tooldir': [], 'funs': None}]
